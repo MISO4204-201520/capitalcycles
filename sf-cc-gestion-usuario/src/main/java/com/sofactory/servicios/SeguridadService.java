50eef342db8094292fe1556227d605a92f8b7a5d
Edgar Camacho <yedgar@gmail.com>
2015-10-08 20:33:44 -0500

    [Gestion Usuario]
    Modificacion servicio rest autenticación usuario

--- sf-cc-gestion-usuario/src/main/java/com/sofactory/servicios/SeguridadService.java
+++ sf-cc-gestion-usuario/src/main/java/com/sofactory/servicios/SeguridadService.java
@@ -1,12 +1,13 @@
 package com.sofactory.servicios;
 
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.sofactory.dtos.RespuestaSeguridadDTO;
+import com.sofactory.dtos.UsuarioDTO;
 import com.sofactory.negocio.interfaces.SeguridadBeanLocal;
 
 @Path("seguridadService")
@@ -16,9 +17,16 @@ public class SeguridadService {
 	private SeguridadBeanLocal seguridadBeanLocal;
 
 	@POST
-	@Path("esValidoUsuario/{login}/{credencial}")
+	@Path("esValidoUsuario")
+	@Consumes("application/json")
 	@Produces("application/json")
-	public RespuestaSeguridadDTO esValidoUsuario(@PathParam("login") String login, @PathParam("credencial") String credencial) {
-		return seguridadBeanLocal.esValidoUsuario(login, credencial);
+	public RespuestaSeguridadDTO esValidoUsuario(UsuarioDTO usuarioDTO) {
+		RespuestaSeguridadDTO respuestaSeguridadDTO = new  RespuestaSeguridadDTO(3, "Campos Obligatorios");
+		if (usuarioDTO!=null && usuarioDTO.getLogin()!=null && !usuarioDTO.getLogin().isEmpty() &&
+				usuarioDTO.getCredencial()!=null && !usuarioDTO.getCredencial().isEmpty()){
+			return seguridadBeanLocal.esValidoUsuario(usuarioDTO.getLogin(), usuarioDTO.getCredencial());	
+		}
+		
+		return respuestaSeguridadDTO;
 	}
 }
