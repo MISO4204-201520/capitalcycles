b7f143455d2551a65ba74ce8e5cc98e66845184f
fergutierrez <fergutierrez55@gmail.com>
2015-10-17 21:18:24 -0500

    [Fidelizacion]
    Autenticación en Fidelizacion

--- sf-cc-fidelizacion/src/main/java/com/sofactory/servicio/FidelizacionServices.java
+++ sf-cc-fidelizacion/src/main/java/com/sofactory/servicio/FidelizacionServices.java
@@ -10,6 +10,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 import com.sofactory.dtos.CatalogoDTO;
@@ -18,12 +22,16 @@ import com.sofactory.dtos.PuntosUsuarioDTO;
 import com.sofactory.dtos.RedimirProductoDTO;
 import com.sofactory.dtos.RegistrarPuntosDTO;
 import com.sofactory.dtos.RespuestaDTO;
+import com.sofactory.dtos.RespuestaSeguridadDTO;
+import com.sofactory.dtos.UsuarioDTO;
 import com.sofactory.entidades.Producto;
 import com.sofactory.negocio.interfaz.FidelizacionBeanLocal;
 
 @Path("fidelizacion")
 public class FidelizacionServices {
 
+	private static String servicioObtenerUsuarioSesion = "http://localhost:8080/sf-cc-gestion-usuario/rest/seguridadService/obtenerUsuarioSesion";
+	
 	@EJB
 	private FidelizacionBeanLocal fidelizacionBean;
 	
@@ -32,13 +40,31 @@ public class FidelizacionServices {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public RespuestaDTO registrarServicio(RegistrarPuntosDTO dto){
+		
 		RespuestaDTO response = new RespuestaDTO();
-		try {
-			fidelizacionBean.registrarServicio(dto);
-			response.setMensaje("OK");
-		}catch (Exception e) {
-			e.printStackTrace();
-			response.setMensaje("ERROR-"+e.getMessage());
+		response.setCodigoUsuario(dto.getCodigoUsuario());
+		
+		UsuarioDTO usuarioDTO = new UsuarioDTO();
+		usuarioDTO.setCodigo(new Long(dto.getCodigoUsuario()));
+	
+		Client client = ClientBuilder.newClient();
+		WebTarget targetMensaje = client.target(servicioObtenerUsuarioSesion);
+		RespuestaSeguridadDTO resu = targetMensaje.request("application/json").post(Entity.entity(usuarioDTO, MediaType.APPLICATION_JSON),RespuestaSeguridadDTO.class);
+		
+		if (resu.getCodigo()==0){
+			
+			try {
+				fidelizacionBean.registrarServicio(dto);
+				response.setMensaje("OK");
+			}catch (Exception e) {
+				e.printStackTrace();
+				response.setMensaje("ERROR-"+e.getMessage());
+			}
+		}else{
+			
+			response.setCodigo(10);
+			response.setMensaje(resu.getMensaje());
+			
 		}
 		
 		return response;
@@ -49,42 +75,78 @@ public class FidelizacionServices {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public RespuestaDTO redimirProducto(RedimirProductoDTO dto){
+		
 		RespuestaDTO response = new RespuestaDTO();
-		try {
-			fidelizacionBean.redimirProducto(dto);
-			response.setMensaje("OK");
-		}catch (Exception e) {
-			e.printStackTrace();
-			response.setMensaje("ERROR-"+e.getMessage());
+		response.setCodigoUsuario(dto.getCodigoUsuario());
+		
+		UsuarioDTO usuarioDTO = new UsuarioDTO();
+		usuarioDTO.setCodigo(new Long(dto.getCodigoUsuario()));
+	
+		Client client = ClientBuilder.newClient();
+		WebTarget targetMensaje = client.target(servicioObtenerUsuarioSesion);
+		RespuestaSeguridadDTO resu = targetMensaje.request("application/json").post(Entity.entity(usuarioDTO, MediaType.APPLICATION_JSON),RespuestaSeguridadDTO.class);
+		
+		if (resu.getCodigo()==0){
+				
+			try {
+				fidelizacionBean.redimirProducto(dto);
+				response.setMensaje("OK");
+			}catch (Exception e) {
+				e.printStackTrace();
+				response.setMensaje("ERROR-"+e.getMessage());
+			}
+		}else{
+			
+			response.setCodigo(10);
+			response.setMensaje(resu.getMensaje());
+			
 		}
 		
 		return response;
 	}
 	
 	@GET
-	@Path("catalogoProdutos")
+	@Path("catalogoProductos/{codigoUsuario}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public CatalogoDTO catalogoProdutos() {
+	public CatalogoDTO catalogoProductos(@PathParam("codigoUsuario")String codigoUsuario) {
+		
 		CatalogoDTO response = new CatalogoDTO();
-		try {
-			List<Producto> producto =
-					fidelizacionBean.obtenerCatalogoProdutos();
+		response.setCodigoUsuario(codigoUsuario);
+
+		UsuarioDTO usuarioDTO = new UsuarioDTO();
+		usuarioDTO.setCodigo(new Long(codigoUsuario));
+	
+		Client client = ClientBuilder.newClient();
+		WebTarget targetMensaje = client.target(servicioObtenerUsuarioSesion);
+		RespuestaSeguridadDTO resu = targetMensaje.request("application/json").post(Entity.entity(usuarioDTO, MediaType.APPLICATION_JSON),RespuestaSeguridadDTO.class);
+		
+		if (resu.getCodigo()==0){
 			
-			response.setProductos(new ArrayList<ProductoDTO>());
-			ProductoDTO dto;
-			for (Producto producto2 : producto) {
-				dto = new ProductoDTO();
-				dto.setId(producto2.getId());
-				dto.setNombre(producto2.getNombre());
-				dto.setPuntos(producto2.getPuntos());
-				response.getProductos().add(dto);
+			try {
+				List<Producto> producto =
+						fidelizacionBean.obtenerCatalogoProdutos();
+				
+				response.setProductos(new ArrayList<ProductoDTO>());
+				ProductoDTO dto;
+				for (Producto producto2 : producto) {
+					dto = new ProductoDTO();
+					dto.setId(producto2.getId());
+					dto.setNombre(producto2.getNombre());
+					dto.setPuntos(producto2.getPuntos());
+					response.getProductos().add(dto);
+				}
+				
+				response.setMensaje("OK");
+			}catch (Exception e) {
+				e.printStackTrace();
+				response.setMensaje("ERROR-"+e.getMessage());
 			}
+		}else{
 			
-			response.setMensaje("OK");
-		}catch (Exception e) {
-			e.printStackTrace();
-			response.setMensaje("ERROR-"+e.getMessage());
+			response.setCodigo(10);
+			response.setMensaje(resu.getMensaje());
+			
 		}
 		
 		return response;
@@ -96,17 +158,36 @@ public class FidelizacionServices {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public PuntosUsuarioDTO obtenerPuntoUsuario
 		(@PathParam("codigoUsuario")String codigoUsuario) {
+	
 		PuntosUsuarioDTO response = new PuntosUsuarioDTO();
-		try {
-			Integer puntos = fidelizacionBean.obtenerPuntosUsuario(codigoUsuario);
-			response.setPuntos(puntos);
-			response.setCodigoUsuario(codigoUsuario);
-			response.setMensaje("OK");
-		}catch (Exception e) {
-			e.printStackTrace();
-			response.setMensaje("ERROR-"+e.getMessage());
+		response.setCodigoUsuario(codigoUsuario);
+		
+		UsuarioDTO usuarioDTO = new UsuarioDTO();
+		usuarioDTO.setCodigo(new Long(codigoUsuario));
+	
+		Client client = ClientBuilder.newClient();
+		WebTarget targetMensaje = client.target(servicioObtenerUsuarioSesion);
+		RespuestaSeguridadDTO resu = targetMensaje.request("application/json").post(Entity.entity(usuarioDTO, MediaType.APPLICATION_JSON),RespuestaSeguridadDTO.class);
+		
+		if (resu.getCodigo()==0){
+		
+			try {
+				Integer puntos = fidelizacionBean.obtenerPuntosUsuario(codigoUsuario);
+				response.setPuntos(puntos);
+				response.setCodigoUsuario(codigoUsuario);
+				response.setMensaje("OK");
+			}catch (Exception e) {
+				e.printStackTrace();
+				response.setMensaje("ERROR-"+e.getMessage());
+			}
+		}else{
+			
+			response.setCodigo(10);
+			response.setMensaje(resu.getMensaje());
+			
 		}
 		
 		return response;
 	}
+	
 }
