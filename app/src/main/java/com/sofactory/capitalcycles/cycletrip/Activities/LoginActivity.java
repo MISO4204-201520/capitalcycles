d808e6e4732b564f97fb9ab2864734e41a5023ba
Luis Sebastian <luchotalcha@hotmail.com>
2015-11-09 00:52:01 -0500

    Soporte login facebook.

--- app/src/main/java/com/sofactory/capitalcycles/cycletrip/Activities/LoginActivity.java
+++ app/src/main/java/com/sofactory/capitalcycles/cycletrip/Activities/LoginActivity.java
@@ -7,12 +7,25 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -24,6 +37,12 @@ import com.sofactory.capitalcycles.cycletrip.Utils.ProgressBar.GenericProgress;
 import com.sofactory.capitalcycles.cycletrip.Utils.Security.JasyptUtils;
 import com.sofactory.capitalcycles.cycletrip.Utils.Services.RegistrationIntentService;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import com.facebook.FacebookSdk;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 /**
  * Created by LuisSebastian on 10/4/15.
@@ -42,12 +61,16 @@ public class LoginActivity extends Activity {
     private String regid;
     private boolean isLogged;
     private SharedPreferences preferences;
-
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private ProfileTracker mProfileTracker;
     private UserLoginTask mAuthTask = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         preferences=getSharedPreferences(USER_PREFERENCES,Context.MODE_PRIVATE);
         isLogged = preferences.getBoolean(UserPreferences.IS_LOGGED,false);
         if(isLogged){
@@ -96,6 +119,81 @@ public class LoginActivity extends Activity {
             Log.i(TAG, "No valid Google Play Services APK found.");
         }
 
+
+        loginButton = (LoginButton) findViewById(R.id.login_button_fb);
+        loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_photos"));
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.e("onSuccess", "--------" + loginResult.getAccessToken());
+                Log.e("Token", "--------" + loginResult.getAccessToken().getToken());
+                Log.e("Permision", "--------" + loginResult.getRecentlyGrantedPermissions());
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+
+                                try {
+                                    String fbId = object.getString("id");
+                                    String birthday = object.getString("birthday");
+                                    String gender = object.getString("gender");
+                                    String email = object.getString("email");
+                                    String link = object.getString("link");
+                                    String name = object.getString("name");
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                Log.i("GraphResponse", "-------------" + response.toString());
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,link,gender,birthday,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast toast = Toast.makeText(getApplicationContext(), "Se cancelo", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Toast toast = Toast.makeText(getApplicationContext(), exception.toString(), Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+        });
+
+
+    }
+
+    private void updateUI() {
+
+        boolean enableButtons = AccessToken.getCurrentAccessToken() != null;
+
+        Profile profile = Profile.getCurrentProfile();
+        if (profile == null) {
+            Log.e("Profile", "null");
+        }
+        if (enableButtons && profile != null) {
+            Log.e("Access Token", AccessToken.getCurrentAccessToken().toString());
+            Log.e("TabSocial", profile.getName());
+        }
     }
 
     /**
@@ -125,7 +223,7 @@ public class LoginActivity extends Activity {
             cancel = true;
         }
 
-        if(cancel){
+        if(cancel) {
             focusView.requestFocus();
         }else{
             GenericProgress genericProgress = new GenericProgress(mProgressView,mLoginFormView,getApplicationContext());
@@ -175,6 +273,11 @@ public class LoginActivity extends Activity {
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 }
